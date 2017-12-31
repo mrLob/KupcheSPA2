62341a21009ebbd3fc6bd5553422310e19b83c51
mister <g12fpay@gmail.com>
2017-12-31 22:19:23 +0300

    скоро будет пагинатор

--- src/app/components/login-dialog/login-dialog.component.ts
+++ src/app/components/login-dialog/login-dialog.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router, ActivatedRoute } from '@angular/router';
+
+import { AlertService } from '../../services/alert.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { User } from '../../shared/models';
 
 @Component({
     selector: 'app-login-dialog',
@@ -7,7 +12,25 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
     styleUrls: ['./login-dialog.component.css']
 })
 export class LoginDialogComponent {
+  public model: User = new User();
+  loading = false;
+  returnUrl: string;
 
-  constructor(public dialogRef: MatDialogRef<LoginDialogComponent>) {}
-
+  constructor(public dialogRef: MatDialogRef<LoginDialogComponent>,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private alertService: AlertService) {}
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model.email || '{}', this.model.pass || '{}')
+        .subscribe(
+            data => {
+                this.router.navigate([this.returnUrl]);
+            },
+            error => {
+                this.alertService.error(error._body);
+                this.loading = false;
+            });
+  }
 }
--- src/app/components/orders/list.orders.component.ts
+++ src/app/components/orders/list.orders.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { DataSource } from '@angular/cdk/collections';
-import { MatPaginator, MatTableDataSource} from '@angular/material';
+import { MatPaginator, MatTableDataSource, PageEvent} from '@angular/material';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
@@ -22,13 +22,27 @@ export class OrdersListComponent implements OnInit {
     @Input('filter')
     public filter: string;
 
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+// MatPaginator Inputs
+    length = 100;
+    pageSize = 10;
+    pageSizeOptions = [5, 10, 25, 100];
+
+    // MatPaginator Output
+    pageEvent: PageEvent;
+
     public displayedColumns = ['caption', 'text', 'cost', 'geomap' ];
-    
 
     constructor(private ordersService: OrdersService) {}
-    public dataSource = new OrderDataSource(this.ordersService);
+    public dataSource = new MatTableDataSource<IOrder>();
+    public orderSource = new OrderDataSource(this.ordersService, this.paginator);
     ngOnInit() {
     }
+    // tslint:disable-next-line:use-life-cycle-interface
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+      }
 }
 export interface IOrder {
     caption?: string;
@@ -38,7 +52,7 @@ export interface IOrder {
 
 }
 export class OrderDataSource extends DataSource<any> {
-    constructor(private ordersService: OrdersService) {
+    constructor(private ordersService: OrdersService, private _paginator: MatPaginator) {
       super();
     }
     connect(): Observable<IOrder[]> {
