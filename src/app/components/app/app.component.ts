e9de8f1050e3c38b7699762d254613128774f89a
mister <g12fpay@gmail.com>
2018-01-12 09:09:22 +0300

    Профиль в тулбаре теперь отдельный компонент

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 import {MatDialog, MatDialogRef} from '@angular/material';
 // tslint:disable-next-line:import-blacklist
 import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../../services/authentication.service';
 import { StorageService } from '../../services/storage.service';
 import { LoginDialogComponent } from '../login-dialog/login-dialog.component';
 import { QuestionDialogComponent } from '../question-dialog/question-dialog.component';
 import { AuthGuard } from '../../_guards/auth.guard';
+
 
 @Component({
     selector: 'app-root',
@@ -23,39 +25,22 @@ export class AppComponent implements OnInit  {
     constructor(private authenticationService: AuthenticationService,
         public localStor: StorageService,
         public authGuard: AuthGuard,
+        private router: Router,
         public dialog: MatDialog) {}
 
     ngOnInit() {
         this.profile = this.localStor.getLocalStorageItem('currentUser');
         console.log(this.profile);
     }
-    tickTack(): void {
-        if (this.profile == null ) {
-            this.profLoad = false;
-            console.log(this.profLoad);
-        }else {
-            this.profLoad = true;
-            console.log(this.profLoad);
-        }
-    }
+
     openDialog(): void {
         const dialog = this.dialog.open(LoginDialogComponent);
         dialog.afterClosed().subscribe(answer => {
             this.profLoad = answer;
-            this.profile = JSON.parse(localStorage.getItem('currentUser'));
         });
     }
-    logoutDialog(): void {
-        const dialog = this.dialog.open(QuestionDialogComponent, {
-            data: { title: 'Предупреждение!', text: 'Хотите выйти?' }
-        });
-        dialog.afterClosed().subscribe(answer => {
-                this.profLoad = !answer;
-                console.log(this.profLoad);
-                if ( answer !== true) {
-                    this.authenticationService.logout();
-                }
-        });
+    getProfile(): any {
+        return JSON.parse(localStorage.getItem('currentUser'));
     }
 
 }
--- src/app/components/profile-bar/profile-bar.component.ts
+++ src/app/components/profile-bar/profile-bar.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {MatDialog, MatDialogRef} from '@angular/material';
+// tslint:disable-next-line:import-blacklist
+import { Observable } from 'rxjs/Rx';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../services/authentication.service';
+import { QuestionDialogComponent } from '../question-dialog/question-dialog.component';
 
 @Component({
   selector: 'app-profile-bar',
   templateUrl: './profile-bar.component.html',
-  styleUrls: ['./profile-bar.component.css']
+  styleUrls: ['./profile-bar.component.css'],
+  providers: [AuthenticationService]
 })
 export class ProfileBarComponent implements OnInit {
 
-  constructor() { }
+  @Input() profile: any;
+  @Output() logout: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private authenticationService: AuthenticationService,
+    private router: Router,
+    public dialog: MatDialog) {}
 
   ngOnInit() {
   }
 
+  logoutDialog(): void {
+    const dialog = this.dialog.open(QuestionDialogComponent, {
+      data: { title: 'Предупреждение!', text: 'Хотите выйти?' }
+    });
+    dialog.afterClosed().subscribe(answer => {
+      if ( answer === true) {
+        this.router.navigate(['/home']);
+        this.authenticationService.logout();
+      }
+      this.logout.emit(answer);
+    });
+  }
 }
